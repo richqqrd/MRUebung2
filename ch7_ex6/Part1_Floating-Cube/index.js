f1805611c69502d7573ab79cbbc87a2c4c6bd46d
richqqrd <spam@richardmiddendorf.de>
2024-11-27 13:22:50 +0100

    Simplify import statement for THREE module in Part1_Floating-Cube/index.js

--- ch7_ex6/Part1_Floating-Cube/index.js
+++ ch7_ex6/Part1_Floating-Cube/index.js
@@ -1,4 +1,4 @@
-import * as THREE from "../../node_modules/three/build/three.module.js";
+import * as THREE from 'three';
 // global scene values
 var btn, gl, glCanvas, camera, scene, renderer, cube;
 
